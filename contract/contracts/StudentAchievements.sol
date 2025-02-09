ea10a4d21656365c254e237bda985fa5df31aec1
Алексей Лемешинский <a.lemeshinsky@gmail.com>
2025-02-09 20:19:20 +0200

    bugfix

--- contract/contracts/StudentAchievements.sol
+++ contract/contracts/StudentAchievements.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract StudentAchievements is ERC721, Ownable {
+contract StudentAchievements is Ownable {
     using Counters for Counters.Counter;
 
     // New enum for submission status.
@@ -24,10 +23,9 @@ contract StudentAchievements is ERC721, Ownable {
         uint256 deadline;
         address[] verifiers;
         uint256 reward;
-        uint256[] submissions;  
+        uint256[] submissions;
     }
 
-    // Updated Submission struct using SubmissionStatus.
     struct Submission {
         address student;
         uint256 projectId;
@@ -39,17 +37,17 @@ contract StudentAchievements is ERC721, Ownable {
 
     Counters.Counter private _projectIds;
     Counters.Counter private _submissionIds;
-    
-    uint256[] public projectIds;  // All projects list
+
+    uint256[] public projectIds; // All projects list
     function getAllProjects() external view returns (uint256[] memory) {
         return projectIds;
     }
 
     mapping(uint256 => Project) public projects;
     mapping(uint256 => Submission) public submissions;
-    
-    mapping(address => uint256[]) public Verifiers;  
-    mapping(address => uint256[]) public InstructorProjectIds;  
+
+    mapping(address => uint256[]) public verifiers;
+    mapping(address => uint256[]) public instructorProjectIds;
     mapping(address => uint256[]) public submissionsByUser;
 
     mapping(address => bool) public isInstructor;
@@ -64,7 +62,6 @@ contract StudentAchievements is ERC721, Ownable {
         address indexed student,
         uint256 projectId
     );
-    // Updated event includes the new status.
     event SubmissionStatusUpdated(
         uint256 indexed submissionId,
         address verifier,
@@ -107,12 +104,10 @@ contract StudentAchievements is ERC721, Ownable {
             submissions: new uint256[](0)
         });
 
-        // Добавляем `projectId` в список проектов инструктора
-        InstructorProjectIds[msg.sender].push(newProjectId);
+        instructorProjectIds[msg.sender].push(newProjectId);
 
-        // Добавляем `projectId` в список проектов для каждого верификатора
         for (uint256 i = 0; i < _verifiers.length; i++) {
-            Verifiers[_verifiers[i]].push(newProjectId);
+            verifiers[_verifiers[i]].push(newProjectId);
         }
 
         emit ProjectCreated(newProjectId, _name, msg.sender);
@@ -120,7 +115,10 @@ contract StudentAchievements is ERC721, Ownable {
         return newProjectId;
     }
 
-    function submitAchievement(uint256 _projectId, string memory _description) external returns (uint256) {
+    function submitAchievement(
+        uint256 _projectId,
+        string memory _description
+    ) external returns (uint256) {
         require(
             projects[_projectId].creator != address(0),
             "Project does not exist"
@@ -129,9 +127,12 @@ contract StudentAchievements is ERC721, Ownable {
             block.timestamp <= projects[_projectId].deadline,
             "Project deadline passed"
         );
-        // instructor can not create submission 
-        require(isInstructor[msg.sender], "Not an instructor");
-        
+        // instructor can not create submission
+        require(
+            !isInstructor[msg.sender],
+            "Instructor cannot submit achievement"
+        );
+
         // check max length
         require(bytes(_description).length <= 300, "Description too long");
         // If a whitelist is provided, ensure the sender is allowed.
@@ -169,7 +170,6 @@ contract StudentAchievements is ERC721, Ownable {
         return newSubmissionId;
     }
 
-    // Updated verifySubmission accepts a boolean to determine approval or rejection.
     function verifySubmission(uint256 _submissionId, bool approve) external {
         Submission storage submission = submissions[_submissionId];
         require(submission.student != address(0), "Submission does not exist");
@@ -193,14 +193,11 @@ contract StudentAchievements is ERC721, Ownable {
             }
         }
         require(isAuthorized, "Not authorized to verify this submission");
-        // If verifier was not set - setup
         if (submission.verifier == address(0)) {
             submission.verifier = msg.sender;
         }
         if (approve) {
             submission.status = SubmissionStatus.Approved;
-            // Optionally mint an NFT certificate for an approved submission.
-            _mint(submission.student, _submissionId);
         } else {
             submission.status = SubmissionStatus.Rejected;
         }
@@ -212,78 +209,23 @@ contract StudentAchievements is ERC721, Ownable {
         );
     }
 
-    // New getter to retrieve the status of a submission.
-    function getSubmissionStatus(uint256 _submissionId)
-        external
-        view
-        returns (SubmissionStatus)
-    {
-        require(
-            submissions[_submissionId].student != address(0),
-            "Submission does not exist"
-        );
-        return submissions[_submissionId].status;
-    }
-
     // Getter to retrieve all submission IDs for a specific user.
-    function getUserSubmissions(address _user)
-        external
-        view
-        returns (uint256[] memory)
-    {
-        return submissionsByUser[_user];
-    }
+    function getUserSubmissions(
+        address _user
+    ) external view returns (Submission[] memory) {
+        uint256[] memory userSubmissionIds = submissionsByUser[_user];
+        Submission[] memory userSubmissions = new Submission[](
+            userSubmissionIds.length
+        );
 
-    // getter for get all list submission by address
-    // Getter to retrieve all submission details by the project creator's address
-    function getSubmissionsByCreator(address _creator) external view returns (Submission[] memory) {
-    // Initialize an array to hold all submissions by the creator
-    uint256[] memory creatorProjectIds = InstructorProjectIds[_creator];
-    uint256 totalSubmissions = 0;
-
-    // Count the total number of submissions by the creator
-    for (uint256 i = 0; i < creatorProjectIds.length; i++) {
-        totalSubmissions += projects[creatorProjectIds[i]].submissions.length;
-    }
-
-    // Create an array to store all the submissions
-    Submission[] memory allSubmissions = new Submission[](totalSubmissions);
-    uint256 index = 0;
-
-    // Iterate over each project created by the instructor
-    for (uint256 i = 0; i < creatorProjectIds.length; i++) {
-        uint256 projectId = creatorProjectIds[i];
-        Project storage project = projects[projectId];
-        
-        // Iterate over all submissions in the project
-        for (uint256 j = 0; j < project.submissions.length; j++) {
-            uint256 submissionId = project.submissions[j];
-            allSubmissions[index] = submissions[submissionId];
-            index++;
+        for (uint256 index = 0; index < userSubmissionIds.length; index++) {
+            uint256 submissionId = userSubmissionIds[index];
+            Submission storage submission = submissions[submissionId];
+            if (submission.student == _user) {
+                userSubmissions.push(submission);
+            }
         }
+
+        return userSubmissions;
     }
-
-    return allSubmissions;
-}
-
-    function getSubmissionDetails(uint256 _submissionId) external view returns (
-        address student,
-        uint256 projectId,
-        SubmissionStatus status,
-        string memory projectName,
-        string memory projectDescription
-) {
-    require(submissions[_submissionId].student != address(0), "Submission does not exist");
-
-    Submission memory submission = submissions[_submissionId];
-    Project memory project = projects[submission.projectId];
-
-    return (
-        submission.student,
-        submission.projectId,
-        submission.status,
-        project.name,
-        project.description
-    );
-}
 }
