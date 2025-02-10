3f7ef7be3c0d2986576bdf22f7bf109e15d4d96f
Алексей Лемешинский <a.lemeshinsky@gmail.com>
2025-02-10 21:29:34 +0200

    useInstructor hook

--- frontend2/src/pages/ProjectsPage.tsx
+++ frontend2/src/pages/ProjectsPage.tsx
@@ -1,31 +1,27 @@
-import { FC } from 'react'
-import { useWallet } from '../hooks/useWallet';
-import Project from '../components/Project';
+import { FC } from "react";
+import { useWallet } from "../hooks/useWallet";
+import Project from "../components/Project";
 
 const projects = [
-    {id: 1, name: "TEST"},
-    {id: 2, name: "TEST2"},
-    {id: 3, name: "TEST3"},
-    {id: 4, name: "TEST4"},
-]
+  { id: 1, name: "TEST" },
+  { id: 2, name: "TEST2" },
+  { id: 3, name: "TEST3" },
+  { id: 4, name: "TEST4" },
+];
 
 const ProjectsPage: FC = () => {
-    
-    const { walletAddress, connectWallet } = useWallet();
+  const { walletAddress, connectWallet } = useWallet();
 
+  return (
+    <div>
+      <h1>Projects</h1>
+      <div>
+        {projects.map((project) => (
+          <Project key={project.id} id={project.id} name={project.name} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-
-    return (
-        <div>
-            <h1>Projects</h1>
-            <div>
-                {projects.map(project => (
-                    <Project key={project.id} id={project.id} name={project.name} />
-                ))}
-            </div>
-
-        </div>
-    )
-}
-
-export default ProjectsPage
+export default ProjectsPage;
