9c332b952865734da15ea34a9284d2afab04a568
Ihor Samartsev <isamarcev1994@gmail.com>
2025-02-11 19:36:14 +0200

    updated contract

--- frontend2/src/config.ts
+++ frontend2/src/config.ts
@@ -2,7 +2,7 @@ import contractAbi from "../abi/chain_contract.json";
 
 export const config = {
   rpcUrl: "https://sepolia.infura.io/v3/c08f6f09d0d0498fb3b5cd74a36eec15",
-  contractAddress: "0x3c3605843f61ea0dd1ed359d55d37cddbfb84d3e",
+  contractAddress: "0xeb2804a0561f1a51e274888dc100f3706ac42716",
   contractAbi: contractAbi,
 };
 
